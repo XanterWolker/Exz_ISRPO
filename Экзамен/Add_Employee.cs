3816a37705d581aa92a36dff3a659202258ede2b
gr691_gaa <toni.goiman.01@mail.ru>
2021-06-28 10:02:02 +0700

    создал класс и переделал таблицу

---
+++ Экзамен/Add_Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Экзамен
+{
+    class Add_Employee
+    {
+        public bool Insert(string LastName, string FirstName, string MiddleName)
+        {
+
+
+
+            return true;
+        }
+    }
+}
