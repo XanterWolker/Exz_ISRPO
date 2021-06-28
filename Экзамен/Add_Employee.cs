65b9b0e024bc818a779e5128fe967405cd525e2b
gr691_gaa <toni.goiman.01@mail.ru>
2021-06-28 10:47:36 +0700

    Написал код в классе

--- Экзамен/Add_Employee.cs
+++ Экзамен/Add_Employee.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Экзамен
 {
@@ -11,7 +21,15 @@ namespace Экзамен
         public bool Insert(string LastName, string FirstName, string MiddleName)
         {
 
+            Database1Entities db = new Database1Entities();
 
+            Table table = new Table();
+            table.Last_Name = LastName;
+            table.First_Name = FirstName;
+            table.Middle_Name = MiddleName;
+
+            db.Table.Add(table);
+            db.SaveChanges();
 
             return true;
         }
